db294f0d714ec530df794c6405aec2a664181a8b
lukakralj <luka.kralj2@gmail.com>
2018-12-08 21:26:27 +0000

    Link sign up page to backend - TODO: test

--- app/src/main/java/com/example/lukak/dragonhunt/SignUpPage.java
+++ app/src/main/java/com/example/lukak/dragonhunt/SignUpPage.java
@@ -5,11 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.lukak.dragonhunt.backend.NewUserRequest;
 
 public class SignUpPage extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private EditText nameInput;
+    private EditText surnameInput;
+    private EditText usernameInput;
+    private EditText passwordInput;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +48,63 @@ public class SignUpPage extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        nameInput = (EditText) findViewById(R.id.signUpName);
+        surnameInput = (EditText) findViewById(R.id.signUpSurname);
+        usernameInput = (EditText) findViewById(R.id.signUpUsername);
+        passwordInput = (EditText) findViewById(R.id.signUpPassword);
+
+        final Button button = (Button) findViewById(R.id.signUpConfirm);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleSignUpConfirmButton(v);
+            }
+        });
+    }
+
+    private void handleSignUpConfirmButton(View v) {
+        boolean allValid = true;
+
+        if (nameInput.getText().toString().trim().equals("")) {
+            allValid = false;
+        }
+        if (surnameInput.getText().toString().trim().equals("")) {
+            allValid = false;
+        }
+        if (usernameInput.getText().toString().trim().equals("") || usernameInput.getText().toString().trim().length() < 5) {
+            allValid = false;
+        }
+        if (passwordInput.getText().toString().length() < 8) {
+            allValid = false;
+        }
+        String message = "";
+        if (allValid) {
+            message = createNewUser();
+        }
+        else {
+            message = "Please fill all the fields. Username must be at least 5 characters long, and password at least 8.";
+        }
+        mTextMessage.setText(message);
+    }
+
+    private String createNewUser() {
+        NewUserRequest newUser = new NewUserRequest();
+        newUser.setName(nameInput.getText().toString().trim());
+        newUser.setSurname(surnameInput.getText().toString().trim());
+        newUser.setUsername(usernameInput.getText().toString().trim());
+        newUser.setPassword(passwordInput.getText().toString().trim());
+
+        if (newUser.issueRequest()) {
+            if (newUser.wasSuccessful()) {
+                return "Registration succesful.";
+            }
+            else {
+                return newUser.getMessage();
+            }
+        }
+        else {
+            return "Exception thrown.";
+        }
     }
 
 }
