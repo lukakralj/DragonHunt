a2a102dcff995a0c994d0d36313bbb204d76e490
smyiyip <yipsy114@gmail.com>
2018-12-08 23:53:39 +0000

    my first commit

--- app/src/main/java/com/example/lukak/dragonhunt/MainActivity.java
+++ app/src/main/java/com/example/lukak/dragonhunt/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.lukak.dragonhunt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        
-    }
+
+        Button search_btn = (Button) findViewById(R.id.button4);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, searchgroup.class));
+            }
+        });
+
+        Button challenge_btn = (Button) findViewById(R.id.button6);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Create_hunt.class));
+            }
+        });
+
+        Button join_btn = (Button) findViewById(R.id.button5);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, join_group.class));
+            }
+        });
+    };
+
 
 }
--- app/src/main/java/com/example/lukak/dragonhunt/join_group.java
+++ app/src/main/java/com/example/lukak/dragonhunt/join_group.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class SignUpPage extends AppCompatActivity {
+public class join_group extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -34,7 +34,7 @@ public class SignUpPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.activity_join_group);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
